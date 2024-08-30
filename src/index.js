ff2fdf039c5f199c78e0035240e1c494c7b0fab6
Aliking314 <appenpvtltd@gmail.com>
2024-08-30 16:41:16 +0500

    created App.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import dotenv from 'dotenv'
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -13,7 +14,14 @@ dotenv.config({
 
 
 
-connectDB()
+connectDB().then(process.env.PORT||8000,()=>{
+    app.listen(()=>{
+        console.log(`App is Listening on ${process.env.PORT}`);
+ })
+})
+.catch((err)=>{
+  console.log("Connection Failed MongoDb",err);
+})
 
 
 
