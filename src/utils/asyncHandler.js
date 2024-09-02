f6abcbfbf7d3d2ff91d2919ea69452c2f0a1605f
Aliking314 <appenpvtltd@gmail.com>
2024-09-02 10:26:28 +0500

    App settings

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+const asyncHandler = (requestHandler)=>{
+  return (req,res,next) => Promise.resolve(requestHandler(req,res,next)).catch((error) => next(error))
+}
+
+export { asyncHandler }
+
+
+// const asyncHandler = (func)=>(req,res,next)=>{
+//     try {
+        
+//     } catch (error) {
+//         res.statusCode(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
